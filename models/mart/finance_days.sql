0572a5832cf91ecef01c155ab7fc2c057eb4eec8
nowdaa390@gmail.com <nowdaa390@gmail.com>
2024-03-06 20:23:31 +0000

    save

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,4 +1,5 @@
-SELECT
+{{ config(materialized='table') }}
+select
     date_date
     ,COUNT(orders_id) AS nb_transactions
     ,ROUND(SUM(revenue),0) AS revenue 
@@ -8,9 +9,9 @@ SELECT
     ,ROUND(SUM(operational_margin),0) AS operational_margin 
     ,ROUND(SUM(purchase_cost),0) AS purchase_cost 
     ,ROUND(SUM(shipping_fee),0) AS shipping_fee 
-    ,ROUND(SUM(logcost),0) AS logcost 
-    ,ROUND(SUM(ship_cost),0) AS ship_cost 
+    ,ROUND(SUM(logcost),0) AS logcost
+    ,round(sum(cast(ship_cost AS float64)),0) AS ship_cost 
     ,SUM(quantity) AS quantity 
-FROM {{ref("int_orders_operational")}}
-GROUP BY  date_date
-ORDER BY  date_date DESC
+FROM {{ ref('int_orders_operational') }}
+group by date_date
+order by date_date desc
